278c793d6fc130ddd4060498a22321a1f71589b7
hambergss <achen200924@gmail.com>
2023-10-02 14:00:21 -0400

    Update Logical Equivalence Problem.py

---
+++ Logical Equivalence Problem.py
@@ -0,0 +1,12 @@
+#if I get 90 or greater for my averages on my grades (x) and took the garbage out (y) then I can get a new ipad
+
+x = input("Enter your average for your grades: ")
+
+y = input("Did you take out the garbage: ")
+
+if x >= 90 and y == True:
+    print("Contrats, you earned a new Ipad!")
+else: 
+    print("Sorry bozo you don't deserve anything")
+
+print("Hello")
