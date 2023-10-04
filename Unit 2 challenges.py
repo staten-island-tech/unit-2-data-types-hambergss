adf053b862b540d56ed45d22ccecadbd83aae7a1
hambergss <achen200924@gmail.com>
2023-10-04 13:51:15 -0400

    Create Unit 2 challenges.py

---
+++ Unit 2 challenges.py
@@ -0,0 +1,30 @@
+# integers and floats
+
+x = 8
+y = float(8)
+print(x,y)
+
+# tip calculator
+
+bill = float(input("Enter your bill: "))
+tip = int(input("Enter your tip: "))
+
+print(bill)
+print(tip)
+print(bill + tip)
+
+# challenge on creating a function that determines if a number is odd or even
+
+x = int(input("Enter a number: "))
+
+if x%2 == 0:
+    print("It is Even")
+else:
+    print("It is Odd")
+
+# challenge on creating a function to accept a "bill" value and offer a tipe of 0%, 15%, 20%, or 25% depending on if the service was "bad, okay, good, or great".
+
+bill = float(input("Enter your bill: "))
+tip = float(input("(0%, 15%, 20%, OR 25%) Enter your tip percentage: "))
+
+print(bill + )
